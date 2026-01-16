435983b9603fb15df55344dfe4b18244fad4665e
FlorisRylant <florisrylant@gmail.com>
2026-01-16 12:24:59 +0100

    market_fetcher gemaakt

---
+++ market_fetcher.py
@@ -0,0 +1,40 @@
+import requests, json
+from colorama import Fore as F
+
+def get_order_book(ticker, market_identifier='NC1', logging=False):
+    try:
+        order_book = requests.get(f'https://rest.fnar.net/exchange/{ticker}.{market_identifier}')
+        order_book.raise_for_status()
+        order_book = order_book.json()
+    except requests.HTTPError as e:
+        print(f'URL ongeldig: {e}')
+    except:
+        print(f'Omzetten naar json mislukt, kijk de URL na.')
+    if logging: print(f'Fetched {F.BLUE+ticker+F.RESET} from {F.BLUE+order_book["ExchangeName"]+F.RESET}.')
+    return order_book
+
+def get_ask(ticker, market_identifier='NC1', logging=False):
+    """Deze functie gaat eerst naar de website via fetch_order_book
+    => enkel gebruiken als je enkel de vraagprijs nodig hebt voor efficiëntie"""
+    return get_order_book(ticker, market_identifier, logging)['Ask']
+
+def get_bid(ticker, market_identifier='NC1', logging=False):
+    """Deze functie gaat eerst naar de website via fetch_order_book
+    => enkel gebruiken als je enkel de aangeboden prijs nodig hebt voor efficiëntie"""
+    return get_order_book(ticker, market_identifier, logging)['Bid']
+
+def get_currency(market_identifier):
+    return {'NC1':'NCC', 'CI1':'CIS', 'IC1':'ICA', 'AI1':'AIC', 'NC2':'NCC', 'CI2':'CIS'}[market_identifier]
+
+def get_order_books(tickers, market_identifier='NC1', logging=False): # voor meerdere dingen
+    if not type(tickers)==set:
+        try:
+            tickers = set(tickers)
+        except:
+            print(f'Kan {tickers} niet omzetten naar een set')
+            return None
+    out = {}
+    for ticker in tickers: out[ticker] = get_order_book(ticker, market_identifier, logging)
+    return out
+
+print(get_order_books(['SF', 'FF']))
